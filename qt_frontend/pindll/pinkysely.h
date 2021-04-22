db4dd52a902cec286cd7094c4ddb112d9569a4c5
luukasmepham <luukasmepham@github.com>
2021-04-22 10:05:55 +0300

    pindll siivottu ja kommentoitu

--- qt_frontend/pindll/pinkysely.h
+++ qt_frontend/pindll/pinkysely.h
@@ -25,27 +25,16 @@ public:
 
 private slots:
     void on_b1_clicked();
-
     void on_b2_clicked();
-
     void on_b3_clicked();
-
     void on_b4_clicked();
-
     void on_b5_clicked();
-
     void on_b6_clicked();
-
     void on_b7_clicked();
-
     void on_b8_clicked();
-
     void on_b9_clicked();
-
     void on_b_c_clicked();
-
     void on_b0_clicked();
-
     void on_b_ok_clicked();
 
 private:
