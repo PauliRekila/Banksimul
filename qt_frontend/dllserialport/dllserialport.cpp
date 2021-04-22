a0067ce0c76e79055c8e2533061a21d04e0fd580
Pauli Rekilä <t0repa00@students.oamk.fi>
2021-04-22 10:05:38 +0300

    serialport siivottu ja kommentoitu

--- qt_frontend/dllserialport/dllserialport.cpp
+++ qt_frontend/dllserialport/dllserialport.cpp
@@ -1,13 +1,13 @@
 #include "dllserialport.h"
 
 Dllserialport::Dllserialport(QObject *parent) : QObject(parent)
-{
+{   
     pEngine = new engine;
+
     connect(pEngine, SIGNAL(signalToInterface(QString)),
             this, SLOT(receiveSignalFromEngine(QString)));
     connect(pEngine, SIGNAL(errToInterface()),
             this, SLOT(receiveErrFromEngine()));
-    qDebug() << "interface luotu";
 }
 
 Dllserialport::~Dllserialport()
