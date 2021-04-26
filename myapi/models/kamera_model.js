6d72f5be793486655b5dfd10ee3e0582ed6f0462
luukasmepham <luukasmepham@github.com>
2021-04-26 10:23:37 +0300

    kamera restapi koodi

---
+++ myapi/models/kamera_model.js
@@ -0,0 +1,13 @@
+const db = require('../database');
+
+const kamera={
+    tallenna_kuva: function(procedure_params, callback) {
+        return db.query(
+          'CALL tallenna_kuva(?,?)',
+          [procedure_params.id, procedure_params.polku],
+          callback
+        );
+      }
+    };
+
+module.exports = kamera;
